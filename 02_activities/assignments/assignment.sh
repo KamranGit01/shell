cb4c41aefd88938788e25aaa430c29b3dd2f2766
Kamran Akbari-Moornani <kamran.akbari.m@hotmail.com>
2025-11-10 04:04:34 -0500

    Resolve merge conflict in assignment.sh

--- 02_activities/assignments/assignment.sh
+++ 02_activities/assignments/assignment.sh
@@ -51,7 +51,7 @@ cp ./data/raw/*event*.log ./data/processed/event_logs/
 rm ./data/raw/*ipaddr* ./data/processed/user_logs/*ipaddr*
 
 # 8. Create a file named ./data/inventory.txt that lists all the files in the subfolders of ./data/processed
-ls -R ./data/processed > ./data/inventory.txt
+find ./data/processed -type f > ./data/inventory.txt
 
 ###########################################
 
